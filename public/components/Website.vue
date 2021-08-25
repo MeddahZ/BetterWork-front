5d078edb8a0a47102ae24d950550239eba9c2a2e
valkyrie <hafsamdh18@gmail.com>
2021-08-25 18:31:33 +0100

    sth

---
+++ public/components/Website.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+name: 'website';
+components: {
+    
+}
+
+
+<style>
+
+</style>
---
+++ public/components/bar.vue
@@ -0,0 +1,127 @@
+<template>
+        <ul class="nav nav-tabs">
+          <li class="active "><a data-toggle="tab" href="#home" class="nav-item nav-link linka"  style="text-decoration: none;">Donations</a></li>
+          <li><a data-toggle="tab" href="#menu1" class="nav-item nav-link linka"  style="text-decoration: none;">compensations</a></li>
+          <li><a data-toggle="tab" href="#menu2" class="nav-item nav-link linka"  style="text-decoration: none;">offers</a></li>
+          <li><a data-toggle="tab" href="#menu3" class="nav-item nav-link linka" style="text-decoration: none;">Debts</a></li>
+        </ul>
+</template>
+<script>rw
+export default {
+    
+}
+</script>
+<style>
+        .btn-grad{
+        background-image: linear-gradient(to right, #e97923 0%, #e97923 30%, #efa81d   100%)}
+         .btn-grad {
+            margin: 10px;
+            padding: 15px 45px;
+            text-align: center;
+            text-transform: uppercase;
+            transition: 0.5s;
+            background-size: 200% auto;
+            color: white;            
+            box-shadow: 0 0 20px #eee;
+            border-radius: 4px;
+            display: block;
+            border: 0px;
+          }
+
+          .btn-grad2:hover {
+            background-color: black; /* change the direction of the change here */
+            color: #fff;
+            text-decoration: none;
+          }
+         
+         .btn-grad2 {
+           background-color: grey;
+            margin: 10px;
+            padding: 15px 45px;
+            text-align: center;
+            text-transform: uppercase;
+            transition: 0.5s;
+            background-size: 200% auto;
+            color: white;            
+            box-shadow: 0 0 20px #eee;
+            border-radius: 4px;
+            display: block;
+            border: 0px;
+          }
+
+          .btn-grad:hover {
+            background-position: right center; /* change the direction of the change here */
+            color: #fff;
+            text-decoration: none;
+          }
+          .linka{
+            text-decoration: underline;
+            color: #e97923;
+          }
+          .linka:hover{
+            color:#efa81d;
+            text-decoration: underline;
+
+          }
+          .search {
+    width: 100%;
+    margin-bottom: auto;
+    margin-top: 20px;
+    height: 50px;
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 5px
+}
+
+.search-input {
+    color: white;
+    border: 0;
+    outline: 0;
+    background: none;
+    width: 0;
+    margin-top: 5px;
+    caret-color: transparent;
+    line-height: 20px;
+    transition: width 0.4s linear
+}
+
+.search .search-input {
+    padding: 0 10px;
+    width: 100%;
+    caret-color: #536bf6;
+    font-size: 19px;
+    font-weight: 300;
+    color: black;
+    transition: width 0.4s linear
+}
+
+.search-icon {
+    height: 34px;
+    width: 34px;
+    float: right;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    background-color: #536bf6;
+    font-size: 10px;
+    bottom: 30px;
+    position: relative;
+    border-radius: 5px
+}
+.uneditable-input:focus {   
+  border-color: #e97923;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.025) inset, 0 0 10px #e97923;
+  outline: 0 none;
+}
+.bootstrap-select .bs-ok-default::after {
+    width: 0.3em;
+    height: 0.6em;
+    border-width: 0 0.1em 0.1em 0;
+    transform: rotate(45deg) translateY(0.5rem);
+}
+
+.btn.dropdown-toggle:focus {
+    outline: none !important;
+}     
+</style>
